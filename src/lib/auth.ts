876b1a7489c7e68f7f321f64f055f6a488708646
Timothée Van Den Bosch <timotheevdbosch@gmail.com>
2026-03-23 12:26:13 +0100

    fix(env-vars): BASE_URL

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,21 +4,32 @@ import { prisma } from '@/lib/prisma';
 import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
 import { nextCookies } from 'better-auth/next-js';
+import { emailOTP } from 'better-auth/plugins';
+
+const otpExpiresIn = Number(process.env.AUTH_EMAIL_OTP_EXPIRES_IN ?? 300);
 
 export const auth = betterAuth({
+  baseURL: process.env.BASE_URL,
   database: prismaAdapter(prisma, {
     provider: 'postgresql',
   }),
   emailAndPassword: {
     enabled: true,
+    autoSignIn: true,
   },
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      prompt: 'select_account',
     },
   },
-  plugins: [nextCookies()],
+  trustedOrigins: [
+    process.env.BASE_URL ?? 'http://localhost:3000',
+  ],
+  plugins: [
+    nextCookies(),
+  ],
   user: {
     additionalFields: {
       role: {
