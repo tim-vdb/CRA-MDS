6b288c1a7b9b6033b8fc6346ef628869cc768f28
Timothée Van Den Bosch <timotheevdbosch@gmail.com>
2026-05-20 08:53:38 +0200

    feat(NativeWind (like Tailwind CSS) added)

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,3 +1,5 @@
+import '../global.css';
+
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
